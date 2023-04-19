604eb8f43db3d937ac30fb8caeb80499ae64ab75
Marc <marc@mustbe.nl>
2023-04-19 13:01:36 +0200

    feat(Makefile): add Makefile to copy MySawHelper.py to Blender addons directory
    This commit adds a Makefile to copy MySawHelper.py to the Blender addons directory. The Makefile includes support for macOS, Linux, and Windows operating systems. The Makefile includes an install target to copy the file to the appropriate directory and a clean target to remove the file from the directory.
    
    style(MySawHelper.py): fix indentation and add spaces around operators and commas

--- MySawHelper.py
+++ MySawHelper.py
@@ -20,63 +20,68 @@ def show_popup_message(title, message):
     def draw(self, context):
         self.layout.label(text=message)
 
-    bpy.context.window_manager.popup_menu(draw, title=title, icon='ERROR')
+    bpy.context.window_manager.popup_menu(draw, title=title, icon="ERROR")
+
 
 def bprint(data):
     for window in bpy.context.window_manager.windows:
         screen = window.screen
         for area in screen.areas:
-            if area.type == 'CONSOLE':
-                override = {'window': window, 'screen': screen, 'area': area}
-                bpy.ops.console.scrollback_append(override, text=str(data), type="OUTPUT")       
+            if area.type == "CONSOLE":
+                override = {"window": window, "screen": screen, "area": area}
+                bpy.ops.console.scrollback_append(
+                    override, text=str(data), type="OUTPUT"
+                )
+
 
 def setViewport():
-    for area in bpy.context.screen.areas: 
-        if area.type == 'VIEW_3D':
-            for space in area.spaces: 
-                if space.type == 'VIEW_3D':
-                    space.shading.type = 'MATERIAL'
+    for area in bpy.context.screen.areas:
+        if area.type == "VIEW_3D":
+            for space in area.spaces:
+                if space.type == "VIEW_3D":
+                    space.shading.type = "MATERIAL"
 
-def createPlank(name,scale,color,x =0, y = 0, z = 0, makematerial = True, center = False):
-    
+
+def createPlank(name, scale, color, x=0, y=0, z=0, makematerial=True, center=False):
     if makematerial:
-        mat = bpy.data.materials.new(name=name+"_material")
+        mat = bpy.data.materials.new(name=name + "_material")
         mat.use_nodes = True
         nodes = mat.node_tree.nodes
         principled_bsdf = nodes.get("Principled BSDF")
         if principled_bsdf is not None:
-            principled_bsdf.inputs["Base Color"].default_value = color  # set the color to red
-    
+            principled_bsdf.inputs[
+                "Base Color"
+            ].default_value = color  # set the color to red
+
     if not center:
-        x = -(scale[0]/2) + x
-        y = -(scale[1]/2) + y
+        x = -(scale[0] / 2) + x
+        y = -(scale[1] / 2) + y
     else:
         x = 0 + x
         y = 0 + y
-            
+
     # Add a new cube mesh
     mesh = bpy.ops.mesh.primitive_cube_add(
         size=1,
         enter_editmode=False,
-        align='WORLD',
+        align="WORLD",
         location=(x, y, z),
         scale=scale,
     )
-    
+
     if makematerial:
         bpy.context.active_object.data.materials.append(mat)
     bpy.context.active_object.name = name
-    
+
     return bpy.context.active_object
 
 
-def createOtherPlanks(mainplank,plank_dims,cut,colors,coll,depth=0.018):
+def createOtherPlanks(mainplank, plank_dims, cut, colors, coll, depth=0.018):
     x, y = 0, 0
 
     savey = 0
 
-    for a,plank_dim in enumerate(plank_dims):
-
+    for a, plank_dim in enumerate(plank_dims):
         abort = False
         # Check if the plank can fit horizontally
         if plank_dim[0] > mainplank[0] or plank_dim[1] > mainplank[1]:
@@ -87,13 +92,20 @@ def createOtherPlanks(mainplank,plank_dims,cut,colors,coll,depth=0.018):
                 if x > 0:
                     x += cut
 
-                plank = createPlank("a_" + str(a),(plank_dim[0],plank_dim[1],depth),tuple(colors[a]), x = -x, y = -y, z = 0)
+                plank = createPlank(
+                    "a_" + str(a),
+                    (plank_dim[0], plank_dim[1], depth),
+                    tuple(colors[a]),
+                    x=-x,
+                    y=-y,
+                    z=0,
+                )
                 removefrom = plank.users_collection
                 coll.objects.link(plank)
                 removefrom[0].objects.unlink(plank)
                 x += plank_dim[0]
-                
-                print(x,y,'added hor')
+
+                print(x, y, "added hor")
 
                 savey = plank_dim[1] + cut
             # Check if the plank can fit vertically
@@ -106,115 +118,126 @@ def createOtherPlanks(mainplank,plank_dims,cut,colors,coll,depth=0.018):
                 else:
                     y += plank_dim[1] + cut
 
-                plank = createPlank("a_" + str(a),(plank_dim[0],plank_dim[1],depth),tuple(colors[a]), x = -x, y = -y, z = 0)
+                plank = createPlank(
+                    "a_" + str(a),
+                    (plank_dim[0], plank_dim[1], depth),
+                    tuple(colors[a]),
+                    x=-x,
+                    y=-y,
+                    z=0,
+                )
                 removefrom = plank.users_collection
                 coll.objects.link(plank)
                 removefrom[0].objects.unlink(plank)
                 x += plank_dim[0]
-                print(x,y,'added ver')
+                print(x, y, "added ver")
             else:
                 print(f"we need another plaat for this {plank_dim}")
                 abort = True
-            
+
         if abort:
             print(f"Can't fit plank with dimensions {plank_dim}")
 
 
-def initBoundries(width,length,height):
-    
-    #coll = bpy.ops.collection.create(name  = "MyTestCollection")
-    #bpy.context.scene.collection.children.link(bpy.data.collections["MyTestCollection"])
-    coll = bpy.data.collections.new(name  = "SawExampleCollection")
+def initBoundries(width, length, height):
+    # coll = bpy.ops.collection.create(name  = "MyTestCollection")
+    # bpy.context.scene.collection.children.link(bpy.data.collections["MyTestCollection"])
+    coll = bpy.data.collections.new(name="SawExampleCollection")
     scene_collection = bpy.context.scene.collection
     scene_collection.children.link(coll)
-    
-    
-    #create some random colors
-    colors_without_alpha = np.round(np.random.rand(2, 3),1)
-    colors = np.column_stack((colors_without_alpha, np.ones(colors_without_alpha.shape[0])))
-    
-    #ref rect
-    blength = math.floor(length)+1
-    bwidth = math.floor(width)+1
-    plank = createPlank('plank01',(blength,bwidth,height),tuple(colors[0]),z = -(height*2))
+
+    # create some random colors
+    colors_without_alpha = np.round(np.random.rand(2, 3), 1)
+    colors = np.column_stack(
+        (colors_without_alpha, np.ones(colors_without_alpha.shape[0]))
+    )
+
+    # ref rect
+    blength = math.floor(length) + 1
+    bwidth = math.floor(width) + 1
+    plank = createPlank(
+        "plank01", (blength, bwidth, height), tuple(colors[0]), z=-(height * 2)
+    )
     removefrom = plank.users_collection
     coll.objects.link(plank)
     removefrom[0].objects.unlink(plank)
-    
-    #bpy.data.collections['Collection'].objects.unlink(plank)
-    
-    plank = createPlank('plank02',(length,width,height),tuple(colors[1]),z = -(height))
+
+    # bpy.data.collections['Collection'].objects.unlink(plank)
+
+    plank = createPlank(
+        "plank02", (length, width, height), tuple(colors[1]), z=-(height)
+    )
     removefrom = plank.users_collection
     coll.objects.link(plank)
     removefrom[0].objects.unlink(plank)
     setViewport()
-    
-    
-    return coll  
+
+    return coll
 
 
-def createPlanks(length,width,height,cut,name):
-    
+def createPlanks(length, width, height, cut, name):
     bprint("Running createPlanks...")
-#     length = 2.44
-#     width = 1.22
-#     height = 0.018
-#     cut = 0.006
-    
-    coll = initBoundries(width,length,height)
-  
+    #     length = 2.44
+    #     width = 1.22
+    #     height = 0.018
+    #     cut = 0.006
+
+    coll = initBoundries(width, length, height)
+
     mainplank = [length, width]
-    
+
     # Get a list of all cube objects in the scene
-    cubes = [obj for obj in bpy.context.scene.objects if obj.type == 'MESH' and obj.name.startswith(name)]
-    
-    #sort cubes by dimensions
+    cubes = [
+        obj
+        for obj in bpy.context.scene.objects
+        if obj.type == "MESH" and obj.name.startswith(name)
+    ]
+
+    # sort cubes by dimensions
     cubes_sorted = sorted(cubes, key=lambda cube: max(cube.dimensions), reverse=True)
-    
+
     plank_dims = []
-        
+
     for a, cube in enumerate(cubes_sorted):
         x, y, z = cube.dimensions
         dimensions = [round(dim, 3) for dim in [x, y, z]]
         dimensions_sorted = sorted(dimensions, reverse=True)
-        
-        #sort by biggest side
+
+        # sort by biggest side
         largest, middle, smallest = dimensions_sorted
-        
+
         b = a + 1
         print(f"Plank '{b}': {largest} m x {middle} m x {smallest} m")
-        
-        #planks to be cut
-        plank_dims.append([largest,middle])
-    
-    
-    #sort by y    
-    plank_dims = sorted(plank_dims, key=lambda x: x[1],reverse=True)
+
+        # planks to be cut
+        plank_dims.append([largest, middle])
+
+    # sort by y
+    plank_dims = sorted(plank_dims, key=lambda x: x[1], reverse=True)
     print(plank_dims)
-    
-    #create some random colors
+
+    # create some random colors
     colors_without_alpha = np.random.rand(len(plank_dims), 3)
-    colors = np.column_stack((colors_without_alpha, np.ones(colors_without_alpha.shape[0])))
-    
-    
-    createOtherPlanks(mainplank,plank_dims,cut,colors,coll,depth = height)
+    colors = np.column_stack(
+        (colors_without_alpha, np.ones(colors_without_alpha.shape[0]))
+    )
 
+    createOtherPlanks(mainplank, plank_dims, cut, colors, coll, depth=height)
 
-    #To run this:
-    #myModule = bpy.data.texts[0].as_module(); myModule.createPlanks();
+    # To run this from the console:
+    # myModule = bpy.data.texts[0].as_module(); myModule.createPlanks();
 
-    # example createPlanks...    
+    # example createPlanks...
     #    length = 1.00
-    #    width = 0.80        
+    #    width = 0.80
     #    createPlank(
-    #                "test03", 
+    #                "test03",
     #                (length,width,0),
-    #                (1,0,0,0), 
-    #                z = 0, 
+    #                (1,0,0,0),
+    #                z = 0,
     #                makematerial = False,
     #                center = True
     #                )
-    
 
 
 class MySawHelper_Props(bpy.types.PropertyGroup):
@@ -231,12 +254,12 @@ class MySawHelper_custom_menu(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Saw helper"
-    
-#     length = 2.44
-#     width = 1.22
-#     height = 0.018
-#     cut = 0.006
-    
+
+    #     length = 2.44
+    #     width = 1.22
+    #     height = 0.018
+    #     cut = 0.006
+
     def draw(self, context):
         layout = self.layout
         props = context.scene.my_props
@@ -248,6 +271,7 @@ class MySawHelper_custom_menu(bpy.types.Panel):
         layout.separator()
         layout.operator("object.mysawhelper_operator", text="Create Saw Example")
 
+
 class MySawHelper_Operator(bpy.types.Operator):
     bl_idname = "object.mysawhelper_operator"
     bl_label = "MySawHelper"
@@ -256,28 +280,34 @@ class MySawHelper_Operator(bpy.types.Operator):
     def execute(self, context):
         bprint("Running MySawHelper...")
         props = context.scene.my_props
-        bprint( str(props.length) + ' - ' +  str(props.width) + ' - ' + str(props.height) + ' - ' + str(props.cut) + ' - ' + str(props.objname) )
+        bprint(
+            str(props.length)
+            + " - "
+            + str(props.width)
+            + " - "
+            + str(props.height)
+            + " - "
+            + str(props.cut)
+            + " - "
+            + str(props.objname)
+        )
         try:
-            createPlanks(props.length,props.width,props.height,props.cut,props.objname)
+            createPlanks(
+                props.length, props.width, props.height, props.cut, props.objname
+            )
             show_popup_message("MySawHelper", "Finished")
         except Exception as e:
-            bprint("Error:", e)
-        return {'FINISHED'}
+            bprint(e)
+        return {"FINISHED"}
 
 
-classes = [
-    MySawHelper_Operator,
-    MySawHelper_custom_menu,
-    MySawHelper_Props
-]
-
-
+classes = [MySawHelper_Operator, MySawHelper_custom_menu, MySawHelper_Props]
 
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    
+
     bpy.types.Scene.my_props = bpy.props.PointerProperty(type=MySawHelper_Props)
 
 
